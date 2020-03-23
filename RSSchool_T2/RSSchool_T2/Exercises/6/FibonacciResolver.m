f553ebb9b23442a30d3203f966acbd8e6e845c12
TimofeyZykov <zikovtm@gmail.com>
2020-03-23 14:47:03 +0300

    refactor: add fibonacci exercise

--- RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
+++ RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
@@ -2,6 +2,16 @@
 
 @implementation FibonacciResolver
 - (NSArray*)productFibonacciSequenceFor:(NSNumber*)number {
-    return @[];
+    NSNumber *first = @0;
+    NSNumber *second = @1;
+    while ([number intValue] >= [first intValue] * [second intValue]) {
+        if ([first intValue] * [second intValue] == [number intValue]) {
+            return @[first, second, @1];
+        }
+        NSNumber *new = [NSNumber numberWithInt:[first intValue]+[second intValue]];
+        first = second;
+        second = new;
+    }
+    return @[first, second, @0];
 }
 @end
