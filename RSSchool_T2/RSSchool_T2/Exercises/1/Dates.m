65fc049ec9098092cab70299fb0a3009f2b310f8
TimofeyZykov <zikovtm@gmail.com>
2020-03-23 12:18:45 +0300

    refactor: add blocks and first excercise

--- RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
+++ RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
@@ -3,7 +3,25 @@
 @implementation Dates
 
 - (NSString *)textForDay:(NSString *)day month:(NSString *)month year:(NSString *)year {
-    return @"";
+    
+    NSDateComponents *dateComponents = [[NSDateComponents alloc] init];
+    
+    dateComponents.day = [day intValue];
+    dateComponents.month = [month intValue];
+    dateComponents.year = [year intValue];
+    
+    NSCalendar* gregorianCalendar = [NSCalendar calendarWithIdentifier:NSCalendarIdentifierGregorian];
+    
+    if ([dateComponents isValidDateInCalendar:gregorianCalendar]) {
+        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+        dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ru_RU"];
+        dateFormatter.dateFormat = @"dd MMMM, EEEE";
+        NSDate *date = [gregorianCalendar dateFromComponents:dateComponents];
+        NSString *returnDate = [dateFormatter stringFromDate:date];
+        return returnDate;
+    } else {
+        return @"Такого дня не существует";
+    }
 }
 
 @end
--- RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
+++ RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
@@ -1,7 +1,30 @@
 #import "Blocks.h"
 
+@interface Blocks () {
+    NSArray *vaultArray;
+}
+
+@end
+
 @implementation Blocks
 
+- (BlockA)blockA {
+    return [^(NSArray *array) {
+        vaultArray = array;
+    } copy];
+}
+
+- (BlockB)blockB {
+    return [^(Class class) {
+        if ([class isKindOfClass:[NSString class]]) {
+            
+        } else if ([class isKindOfClass:[NSNumber class]]) {
+            
+        } else if ([class isKindOfClass:[NSDate class]]) {
+            
+        }
+    } copy];
+}
 
 @end
 
